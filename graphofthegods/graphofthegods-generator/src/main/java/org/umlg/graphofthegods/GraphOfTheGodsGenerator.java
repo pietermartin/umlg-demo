0221d3bf3cb2fb32f2033d3830eabff049bb28af
Pieter Martin <pieter.martin@gmail.com>
2014-08-09 11:23:30 +0200

    moved demos to own project

---
+++ graphofthegods/graphofthegods-generator/src/main/java/org/umlg/graphofthegods/GraphOfTheGodsGenerator.java
@@ -0,0 +1,29 @@
+package org.umlg.graphofthegods;
+
+import org.umlg.generation.JavaGenerator;
+import org.umlg.restlet.generation.RestletVisitors;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+/**
+ * Date: 2014/01/13
+ * Time: 9:19 PM
+ */
+public class GraphOfTheGodsGenerator {
+
+    public static void main(String[] args) throws URISyntaxException {
+        if (args.length == 0) {
+            args = new String[]{"."};
+        }
+        JavaGenerator javaGenerator = new JavaGenerator();
+        javaGenerator.generate(
+                new File(args[0] + "/graphofthegods/graphofthegods-application/graphofthegods-entities/src/main/model/graphofthegods.uml"),
+                new File(args[0] + "/graphofthegods/graphofthegods-application/graphofthegods-entities"),
+                new File(args[0] + "/graphofthegods/graphofthegods-application/graphofthegods-restlet"),
+                RestletVisitors.getDefaultJavaVisitors());
+
+        ///demo/graphofthegods
+    }
+
+}
